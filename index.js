fe05cb616e278546029465b694f21c78a2e961ff
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-21 21:24:18 +0300

    input numero uno

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const filmsList = document.getElementById("films");
+    const poster = document.getElementById("poster");
+    const title = document.getElementById("title");
+    const runtime = document.getElementById("runtime");
+    const showtime = document.getElementById("GET-TICKETS");
+    const butTicketButton = document.getElementById("buy-ticket");
+
+
+    const selectedMovieId = null;
+
+
+
+})
